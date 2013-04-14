f8b7614c11e98460bfecd0854722d370175a4f0f
Denis Rosset <rossetd0@etu.unige.ch>
2013-04-15 01:48:11 +0200

    Refactoring in progress of CGT stuff.

--- src/main/scala/perm/bsgs/package.scala
+++
@@ -1,5 +0,0 @@
-package com.faacets.perm
-
-package object bsgs {
-  implicit def emptyExplicitTransversal[P <: Permutation[P]](beta: Domain, id: P): ExplicitTransversal[P] = ExplicitTransversal(beta, id)
-}
