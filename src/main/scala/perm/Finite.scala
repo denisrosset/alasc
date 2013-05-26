d8d64b1e8dc43cc6791d175f1558613a3fd9f94e
Denis Rosset <rossetd0@etu.unige.ch>
2013-05-26 21:17:09 +0200

    Method in alphabetical order.

--- src/main/scala/perm/Finite.scala
+++ src/main/scala/perm/Finite.scala
@@ -16,13 +16,24 @@ trait FiniteElement[E <: FiniteElement[E]] extends Any with FiniteElementLike {
 }
 
 trait FiniteGroup[E <: FiniteElement[E]] extends Any with HasTeX {
+  /** Tests whether element e is compatible with group structure. */
+  def compatible(e: E): Boolean
+  /** Tests if e is contained in this group. */
+  def contains(e: E): Boolean
+  /** Iterates through the group elements. */
+  def elements: Iterator[E]
+  /** Iterates through the group generators. */
+  def generators: Iterator[E]
+  /** Identity element of this group. */
+  def identity: E
+  /** Order of this group. */
+  def order: BigInt
+  /** Generates a random group element.
+    * 
+    * @param gen Instance of random generator to use.
+    */
+  def random(implicit gen: Random): E
+
   def directCopies(n: Int)(implicit c: ClassTag[E]) = new wreath.BaseGroup[FiniteGroup[E], E](this, n)
   def elementClassTag: ClassTag[E] = ClassTag[E](identity.getClass)
-  def identity: E
-  def order: BigInt
-  def compatible(e: E): Boolean
-  def contains(e: E): Boolean
-  def random(implicit gen: Random): E
-  def elements: Iterator[E]
-  def generators: Iterator[E]
 }
