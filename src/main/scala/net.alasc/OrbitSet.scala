300e46e901dda48de991a6128c18ca76974d0a96
Denis Rosset <rossetd0@etu.unige.ch>
2013-10-13 23:19:09 +0200

    Reimplementation of OrbitSet using collection.BitSet.

--- src/main/scala/net.alasc/OrbitSet.scala
+++ src/main/scala/net.alasc/OrbitSet.scala
@@ -1,30 +1,36 @@
 package net.alasc
 
-case class OrbitSet[F <: FiniteElementLike](beta: Dom, action: Action[F], orbit: Set[Dom]) extends Orbit[F] {
+case class OrbitSet[F <: FiniteElementLike](beta: Dom, action: Action[F], intOrbit: collection.immutable.BitSet) extends Orbit[F] {
+  def orbit = intOrbit.map(k => Dom._0(k))
   def builder = OrbitSet
-  def size = orbit.size
-  def isDefinedAt(k: Dom) = orbit(k)
+  def size = intOrbit.size
+  def isDefinedAt(k: Dom) = intOrbit(k._0)
   def updated(newGen: Iterable[F], gens: Iterable[F]): OrbitSet[F] = {
-    val newPoints = Set.empty[Dom] ++ (
-      for (k <- orbit; g <- newGen; img = action(g, k) if !orbit.contains(img)) yield img
-    )
+    val newOrbit = collection.mutable.BitSet.empty
+    for (k <- intOrbit; g <- newGen)
+      newOrbit += action(g, Dom._0(k))._0
+    val newPoints = newOrbit.clone
+    for (k <- intOrbit)
+      newPoints -= k
     if (newPoints.isEmpty)
       return this
-    var newOrbit = orbit ++ newPoints
     def checkForNew: Boolean = {
       val ret = false
-      for (k <- newOrbit; g <- gens; img = action(g, k) if !newOrbit.contains(img)) {
-        newOrbit += img
-        return true
+      for (k <- newOrbit; g <- gens) {
+        val img = action(g, Dom._0(k))._0
+        if (!newOrbit.contains(img)) {
+          newOrbit += img
+          return true
+        }
       }
       false
     }
     while (checkForNew) { }
-    OrbitSet(beta, action, newOrbit)
+    OrbitSet(beta, action, newOrbit.toImmutable)
   }
 }
 
 object OrbitSet extends OrbitBuilder {
   def empty[F <: FiniteElementLike](beta: Dom, action: Action[F]) = 
-    OrbitSet(beta, action, Set(beta))
+    OrbitSet(beta, action, collection.immutable.BitSet(beta._0))
 }
