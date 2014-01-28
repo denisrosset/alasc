d6848e2fb3b4253178a70c7ba9cd3f10ee1e78e6
Denis Rosset <rossetd0@etu.unige.ch>
2014-01-28 17:14:08 +0100

    Removed wreath group support in alasc.

--- src/main/scala/net.alasc/wreath/WreathAction.scala
+++
@@ -1,8 +0,0 @@
-package net.alasc
-package wreath
-
-trait WreathAction[AE <: FiniteElement[AE], HE <: PermElement[HE]] extends Action[WreathElement[AE,HE]] {
-  def ba: Action[AE]
-  def identity: WreathElement[AE, HE]
-  def faithful = ba.faithful
-}
