e29997084541806450cc0a8b38af28a5aa9d2d51
Denis Rosset <denis.rosset@unige.ch>
2014-07-13 10:53:19 +0200

    Extracted helper method computeIntegerSeq.

--- src/main/scala/net.alasc/LexicoImpl.scala
+++ src/main/scala/net.alasc/LexicoImpl.scala
@@ -6,14 +6,24 @@ import spire.syntax.OrderOps
 import permutable.{Permutable, PermutableImpl}
 import indexSyntax._
 
+object LexicoImpl {
+  def computeIntegerSeq[A: Order](seq: IndexedSeq[A]): IndexedSeq[Int] = {
+    import spire.algebra.Order.ordering
+    val elementMap = seq.distinct.sorted.zipWithIndex.toMap
+    seq.map(elementMap(_))
+  }
+}
+
 trait LexicoImpl[P] extends Lexico[P] {
   self =>
   def symmetryGroup(p: P) = baseGroup(p).fixing(integerSeq(p))
-  def integerSeq(p: P): IndexedSeq[Int] = {
-    val indexedSeq: IndexedSeq[A] = p.indexToIndexedSeq
-    val elementMap = indexedSeq.distinct.sorted(Order.ordering(order)).zipWithIndex.toMap
-    indexedSeq.map(elementMap(_))
-  }
+
+  /** The `integerSeq` is the sequence used for the lexicographic ordering.
+    * Instead of dealing with coefficients of type A, we work with
+    * their relative orders expressed as integers.
+    */
+  def integerSeq(p: P): IndexedSeq[Int] = LexicoImpl.computeIntegerSeq(p.indexToIndexedSeq)
+
   trait PermutableTrait[PP <: PermutableTrait[PP]] extends Permutable[PP, F] {
     def p: P
     def build(newP: P): PP
