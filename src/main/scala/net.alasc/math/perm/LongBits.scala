95a0cc35ec31412704b0fd0c3eb4db30926f9399
Denis Rosset <denis.rosset@unige.ch>
2014-08-10 11:52:24 +0200

    Moved LongBits to net.alasc.util.

--- src/main/scala/net.alasc/math/perm/LongBits.scala
+++
@@ -1,7 +0,0 @@
-package net.alasc.math
-package perm
-
-object LongBits {
-  @inline final def rightFill(n: Int): Long = ((1L << n) - 1) - ((n & 64) >> 6)
-  @inline final def leftFill(n: Int): Long = ~rightFill(64 - n)
-}
