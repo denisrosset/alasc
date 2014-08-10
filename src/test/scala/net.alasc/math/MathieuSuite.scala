8a3014096f4c9074fa5a52c49b004769671c3285
Denis Rosset <denis.rosset@unige.ch>
2014-08-10 22:19:19 +0200

    Mathieu groups test cases.

---
+++ src/test/scala/net.alasc/math/MathieuSuite.scala
@@ -0,0 +1,39 @@
+package net.alasc.math
+
+import org.scalatest.{FunSuite, NonImplicitAssertions, Matchers, EqMatchers}
+import bsgs._
+import net.alasc.syntax.subgroup._
+
+object MathieuGroups {
+  val M11 = Seq(Perm(1,2,3,4,5,6,7,8,9,10,11), Perm(3,7,11,8)(4,10,5,6)) -> BigInt(7920)
+  val M12 = Seq(Perm(1,2,3,4,5,6,7,8,9,10,11), Perm(1,12)(2,11)(3,6)(4,8)(5,9)(7,10), Perm(3,7,11,8)(4,10,5,6)) -> BigInt(95040)
+  val M24 = Seq(Perm("0123456789ABCDEFGHIJKLM")("N"),
+    Perm("0N")("1M")("2B")("3F")("4H")("59")("6J")("7D")("8K")("AG")("CL")("EI"),
+    Perm("2G968")("3CDI4")("7HABM")("EJLKF")) -> BigInt(244823040)
+  val all: Seq[(Seq[Perm], BigInt)] = Seq(M11, M12, M24)
+}
+
+class MathieuSuite extends FunSuite with NonImplicitAssertions with Matchers {
+  test("Mathieu group construction is correct for M11, M12 and M24, using deterministic Schreier-Sims") {
+    import BSGSOptions.deterministic
+    MathieuGroups.all.foreach {
+      case (gens, order) =>
+        val builder = BSGSBuilder.fromGenerators[Perm](gens)
+        builder.completeStrongGenerators
+        builder.chain.order shouldBe order
+    }
+  }
+
+  test("Mathieu group construction works using randomized Schreier-Sims") {
+    import BSGSOptions.randomized
+    MathieuGroups.all.foreach {
+      case (gens, order) =>
+        val builder = new BSGSBuilder[Perm]
+        val bag = RandomBag(gens)
+        while (builder.chain.order < order)
+          for ( (node, p) <- builder.siftAndUpdateBase(bag.random(scala.util.Random)) )
+            node.addStrongGeneratorHere(p)
+        builder.chain.order shouldBe order
+    }
+  }
+}
