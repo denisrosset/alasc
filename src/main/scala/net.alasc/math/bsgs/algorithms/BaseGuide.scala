52550dd884e0fa45aae128cf726c6eb9449862e8
Denis Rosset <denis.rosset@unige.ch>
2014-08-26 15:56:14 +0200

    Scaladoc.

--- src/main/scala/net.alasc/math/bsgs/algorithms/BaseGuide.scala
+++ src/main/scala/net.alasc/math/bsgs/algorithms/BaseGuide.scala
@@ -4,7 +4,20 @@ package algorithms
 
 import net.alasc.algebra.{PermutationAction, Subgroup}
 
+/** Advisor for base changes. The base change algorithm is authorized to ignore the advice. */
 trait BaseGuide {
+  /** Determines the next base point.
+    * 
+    * @param easyPoints Set of points that are easier for the base change.
+    * 
+    * @return The next base point, taken from `easyPoints` whenever possible.
+    */
   def basePoint(easyPoints: collection.Set[Int]): Int
+
+  /** Moves to the next base point, telling this advisor what point was actually used in the base change.
+    * 
+    * @param chosenPoint The base point actually used in the base change.
+    * @param nextGenerators Generators for the next group in the stabilizer chain.
+    */
   def moveToNext[P](chosenPoint: Int, nextGenerators: Iterable[P])(implicit action: PermutationAction[P]): Unit
 }
