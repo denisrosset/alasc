f964f85248c3f6f4efe14ab6242e4a6dfa5f0973
Denis Rosset <denis.rosset@unige.ch>
2014-08-27 16:35:01 +0200

    Added lexicographic order for permutations.

---
+++ src/main/scala/net.alasc/optional/lexPermutationOrder.scala
@@ -0,0 +1,27 @@
+package net.alasc.optional
+
+import spire.algebra.Order
+import spire.syntax.groupAction._
+import net.alasc.algebra.{FiniteGroup, PermutationAction}
+import net.alasc.syntax.permutationAction._
+
+object lexPermutationOrder {
+  /** Lexicographic order on permutations. */
+  class LexPermutationOrder[P](implicit algebra: FiniteGroup[P], action: PermutationAction[P]) extends Order[P] {
+    override def eqv(x: P, y: P) = algebra.eqv(x, y)
+
+    def compare(x: P, y: P): Int = {
+      val n = x.supportMax.getOrElse(-1).max(y.supportMax.getOrElse(-1)) + 1
+      var i = 0
+      while (i < n) {
+        val c = ((i <|+| x) - (i <|+| y)).signum
+        if (c != 0)
+          return c
+        i += 1
+      }
+      0
+    }
+  }
+
+  implicit def LexPermutationOrder[P: FiniteGroup: PermutationAction]: Order[P] = new LexPermutationOrder[P]
+}
--- src/test/scala/net.alasc/math/PermsSuite.scala
+++ src/test/scala/net.alasc/math/PermsSuite.scala
@@ -3,6 +3,7 @@ package net.alasc.math
 import org.scalatest.{FunSuite, NonImplicitAssertions, Matchers, EqMatchers}
 import bsgs._
 import net.alasc.syntax.subgroup._
+import spire.syntax.order._
 
 class PermsSuite extends FunSuite with NonImplicitAssertions with Matchers {
   test("Perms(n).iterator, Perms(n).apply(...) and Sym(n).elements.toSet return the same elements for n = 2,3,4,5,6") {
@@ -10,6 +11,8 @@ class PermsSuite extends FunSuite with NonImplicitAssertions with Matchers {
       val sym = Sym[Perm](n)
       val perms = Perms(n)
       val seqFromPermsIterator = perms.iterator.toSeq
+      import net.alasc.optional.lexPermutationOrder._
+      assert((seqFromPermsIterator.iterator zip seqFromPermsIterator.iterator.drop(1)).forall { case (x, y) => (x < y) })
       val seqFromPermsApply = (0 until perms.length.toInt).map(perms(_)).toSeq
       val setFromSym = sym.elements.toSet
       for (i <- 0 until perms.length.toInt) {
