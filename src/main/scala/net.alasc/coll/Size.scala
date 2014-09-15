4c28e1d52daaf39e98a097df84f308ae6c1bf42d
Denis Rosset <denis.rosset@unige.ch>
2014-09-15 09:13:27 +0200

    coll.Size.toInt

--- src/main/scala/net.alasc/coll/Size.scala
+++ src/main/scala/net.alasc/coll/Size.scala
@@ -1,7 +1,17 @@
 package net.alasc.coll
 
-sealed trait Size
-case class IntSize(size: Int) extends Size
-case class BigIntSize(size: BigInt) extends Size
-case object InfiniteSize extends Size
-
+sealed trait Size {
+  def toInt: Int
+}
+case class IntSize(size: Int) extends Size {
+  def toInt = size
+}
+case class BigIntSize(size: BigInt) extends Size {
+  def toInt = {
+    require(size.isValidInt)
+    size.toInt
+  }
+}
+case object InfiniteSize extends Size {
+  def toInt = sys.error("Cannot return infinite size as Int")
+}
