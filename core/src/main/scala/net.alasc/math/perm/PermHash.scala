f138f9ba29a015e19b90fa0aa35d442423a307ce
Denis Rosset <denis.rosset@unige.ch>
2014-11-14 22:09:11 +0100

    Switched from for to cforRange.

--- core/src/main/scala/net.alasc/math/perm/PermHash.scala
+++ core/src/main/scala/net.alasc/math/perm/PermHash.scala
@@ -2,7 +2,10 @@ package net.alasc.math
 package perm
 
 import scala.util.hashing.MurmurHash3.{orderedHash, unorderedHash}
+
 import spire.syntax.groupAction._
+import spire.syntax.cfor._
+
 import net.alasc.syntax.permutationAction._
 import net.alasc.algebra.Permutation
 
@@ -28,18 +31,21 @@ object PermHash {
   def hash16[P: Permutation](p: P): Int = {
     var encoding0to7 = 0
     var encoding8to15 = 0
-    for (i <- 0 until 7)
+    cforRange(0 until 7) { i =>
       encoding0to7 += (((i <|+| p) - i) & 0xF) << (i * 4)
-    for (i <- 8 until 15)
+    }
+    cforRange(8 until 15) { i =>
       encoding8to15 += (((i <|+| p) - i) & 0xF) << (i * 4)
+    }
     encoding0to7 ^ encoding8to15
   }
 
   def hash32[P: Permutation](p: P): Int = {
     @inline def encode(start: Int, length: Int = 6) = {
       var res = 0
-      for (i <- start until start + length)
+      cforRange(start until start + length) { i =>
         res += (((i <|+| p) - i) & 0x1F) << ((i - start) * 5)
+      }
       res
     }
     import scala.util.hashing.MurmurHash3.{mix, finalizeHash}
