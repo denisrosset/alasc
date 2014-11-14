4cec251744415c0207641288e82a9b18453ef72c
Denis Rosset <denis.rosset@unige.ch>
2014-11-14 21:14:22 +0100

    Changed Lattice to Spire interface (hackish now).

--- core/src/main/scala/net.alasc/algebra/Lattice.scala
+++ core/src/main/scala/net.alasc/algebra/Lattice.scala
@@ -1,18 +1,6 @@
 package net.alasc.algebra
 
-import spire.algebra.PartialOrder
+trait Lattice[A] extends spire.algebra.PartialOrder[A] with spire.algebra.lattice.Lattice[A]
+trait BoundedBelowLattice[A] extends Lattice[A] with spire.algebra.lattice.BoundedJoinSemilattice[A]
+trait BoundedLattice[A] extends BoundedBelowLattice[A] with spire.algebra.lattice.BoundedLattice[A]
 
-trait Lattice[A] extends PartialOrder[A] {
-  def join(x: A, y: A): A
-  def meet(x: A, y: A): A
-}
-
-trait BoundedBelowLattice[A] extends Lattice[A] {
-  def zero: A
-  def isZero(a: A) = eqv(zero, a)
-}
-
-trait BoundedLattice[A] extends BoundedBelowLattice[A] {
-  def one: A
-  def isOne(a: A) = eqv(one, a)
-}
