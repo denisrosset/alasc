2a2a6389a4a901018b4749d3d40dacbde0bd99b9
Denis Rosset <physics@denisrosset.com>
2016-12-13 22:51:37 +0800

    Grp methods to find permutation-defined subgroups for an arbitrary action.

--- core/src/main/scala/net/alasc/finite/Grp.scala
+++ core/src/main/scala/net/alasc/finite/Grp.scala
@@ -12,7 +12,7 @@ import spire.util.Opt
 
 import net.alasc.algebra.{BigIndexedSeq, PermutationAction}
 import net.alasc.domains.Partition
-import net.alasc.perms.{Perm, PermGrpBuilder}
+import net.alasc.perms.{Perm, PermGrpBuilder, PermutationActionGrpBuilder}
 import net.alasc.syntax.all._
 import net.alasc.util.{NNOption, _}
 import metal.syntax._
@@ -85,6 +85,125 @@ abstract class Grp[G] { lhs =>
 
   /** Simplifies the description current group.*/
   def smallGeneratingSet(implicit builder: GrpBuilder[G]): IndexedSeq[G] = builder.smallGeneratingSet(lhs)
+
+  /** Sequence of the group elements, ordered lexicographically by their images.
+    * Returns a value only if the action is faithful. */
+  def lexElements(action: PermutationAction[G])(implicit builder: PermutationActionGrpBuilder[G]): Opt[BigIndexedSeq[G]]
+
+  /** Returns the subgroup that fixes the given partition under the given action. */
+  def fixingPartition(action: PermutationAction[G], partition: Partition)(implicit builder: PermutationActionGrpBuilder[G]): Grp[G]
+
+  /** Returns the subgroup that stabilizes `b` by the given action. */
+  def stabilizer(action: PermutationAction[G], b: Int)(implicit builder: PermutationActionGrpBuilder[G]): Grp[G]
+
+  /** If the action of this group is trivial, returns Opt.empty, otherwise, returns a subgroup that stabilizes some point,
+    * and the associated transversal.
+    */
+  def someStabilizerTransversal(action: PermutationAction[G])(implicit builder: PermutationActionGrpBuilder[G]): Opt[(Grp[G], bsgs.Transversal[G, action.type])]
+
+  /** Returns the subgroup that stabilizes `b` under the given action and the associated transversal. */
+  def stabilizerTransversal(action: PermutationAction[G], b: Int)(implicit builder: PermutationActionGrpBuilder[G]): (Grp[G], bsgs.Transversal[G, action.type])
+
+  def pointwiseStabilizer(action: PermutationAction[G], set: Set[Int])(implicit builder: PermutationActionGrpBuilder[G]): Grp[G]
+
+  def pointwiseStabilizer(action: PermutationAction[G], points: Int*)(implicit builder: PermutationActionGrpBuilder[G]): Grp[G] =
+    pointwiseStabilizer(action, scala.collection.immutable.BitSet(points: _*))
+
+  def setwiseStabilizer(action: PermutationAction[G], set: Set[Int])(implicit builder: PermutationActionGrpBuilder[G]): Grp[G]
+
+  def setwiseStabilizer(action: PermutationAction[G], points: Int *)(implicit builder: PermutationActionGrpBuilder[G]): Grp[G] =
+    setwiseStabilizer(action, scala.collection.immutable.BitSet(points: _*))
+
+  /** Finds an element of this group under `actionG` with the same image as `q` under `actionQ`, if it exists. */
+  def find[Q:Eq:Group](actionG: PermutationAction[G], actionQ: PermutationAction[Q], q: Q)(implicit builder: PermutationActionGrpBuilder[G]): Opt[G]
+
+  /** Finds an element of this group under `actionG` with the same image as the given permutation, if it exists. */
+  def find(actionG: PermutationAction[G], p: Perm)(implicit builder: PermutationActionGrpBuilder[G]): Opt[G]
+
+  /** Returns the subgroup for which `predicate` is satisfied; the test `backtrackTest` is used to
+    * prune the search tree.
+    *
+    * @param action        Action used in the subgroup test
+    * @param backtrackTest Tests if a pair (preimage, image) is valid for an element of the subgroup. False
+    *                      positives are allowed, but a false negative would incorrectly prune the tree.
+    * @param predicate Tests if an element is member of the subgroup
+    * @return the subgroup satisfying `predicate`
+    */
+  def subgroupFor(action: PermutationAction[G], backtrackTest: (Int, Int) => Boolean, predicate: Perm => Boolean)
+                 (implicit builder: PermutationActionGrpBuilder[G]): Grp[G]
+
+  /** Returns a sequence of domain elements such that no element of this group apart from
+    * the identity fixes all the points in the sequence.
+    * Returns a value if and only if the action is faithful.
+    */
+  def base(action: PermutationAction[G])(implicit builder: PermutationActionGrpBuilder[G]): Opt[Seq[Int]]
+
+  /** Return the smallest element of the domain moved by this group under the given action, or [[NNNone]]. */
+  def smallestMovedPoint(action: PermutationAction[G]): NNOption =
+    if (lhs.isTrivial) NNNone else {
+      var mn = Int.MinValue
+      lhs.generators.foreach { g =>
+        action.smallestMovedPoint(g) match {
+          case NNOption(i) =>
+            if (mn == Int.MinValue)
+              mn = i
+            else
+              mn = spire.math.min(i, mn)
+          case _ =>
+        }
+      }
+      if (mn == Int.MinValue) NNNone else NNOption(mn)
+    }
+
+  /** Return the largest element of the domain moved by this group under the given action, or [[NNNone]]. */
+  def largestMovedPoint(action: PermutationAction[G]): NNOption =
+    if (lhs.isTrivial) NNNone else {
+      var mx = Int.MinValue
+      lhs.generators.foreach { g =>
+        mx = spire.math.max(action.largestMovedPoint(g).getOrElse(Int.MinValue), mx)
+      }
+      if (mx == Int.MinValue) NNNone else NNOption(mx)
+    }
+
+  /** Returns an arbitrary element moved by this group under the given action, or [[NNNone]]. */
+  def findMovedPoint(action: PermutationAction[G]): NNOption =
+    if (lhs.isTrivial) NNNone else {
+      lhs.generators.foreach { g =>
+        action.findMovedPoint(g) match {
+          case NNOption(i) => return NNOption(i)
+          case _ =>
+        }
+      }
+      NNNone
+    }
+
+  /** Tests if the point `i` is in the support of `g` under the given action. */
+  def movesPoint(action: PermutationAction[G], i: Int): Boolean = lhs.generators.exists(g => action.actr(i, g) != i)
+
+  /** Number of non-negative integers moved by the permutations in this group. */
+  def nMovedPoints(action: PermutationAction[G]): Int = movedPoints(action).size
+
+  /** Returns a bit set of all non-negative integers k that are moved by the action of this group,
+    * i.e. `S = { k | exists g in this group s.t. k <|+| g != k }`.
+    */
+  def movedPoints(action: PermutationAction[G]): Set[Int] =
+    if (lhs.isTrivial) Set.empty[Int] else {
+      val b = metal.mutable.ResizableBitSet.empty
+      lhs.generators.foreach { g =>
+        action.smallestMovedPoint(g) match {
+          case NNOption(lb) =>
+            cforRange(lb until action.largestMovedPoint(g).get + 1) { i =>
+              if (action.actr(i, g) != i)
+                b += i
+            }
+          case _ =>
+        }
+      }
+      b.toScala
+    }
+
+  def toPerm(action: PermutationAction[G])(implicit builder: PermGrpBuilder): Grp[Perm]
+// TODO if action is not faithful    Grp.fromGeneratorsAndOrder(generators.map(action.toPerm(_)), order)
 
 }
 
@@ -168,7 +287,7 @@ class PermGrpSyntax(val lhs: Grp[Perm]) extends AnyVal {
     if (lhs.isTrivial) NNNone else lhs.generators.head.findMovedPoint // non-empty because generator cannot be the identity
 
   /** Tests if the point `i` is in the support of `g`. */
-  def movesPoint(i: Int): Boolean = lhs.generators.exists(g => i <|+| g != g)
+  def movesPoint(i: Int): Boolean = lhs.generators.exists(g => (i <|+| g) != i)
 
   /** Number of non-negative integers moved by the permutations in this group. */
   def nMovedPoints: Int = movedPoints.size
@@ -188,9 +307,6 @@ class PermGrpSyntax(val lhs: Grp[Perm]) extends AnyVal {
       b.toScala
     }
 
-/* TODO  def toPerm(implicit ev: PermutationAction[G]): Grp[Q] =
-    Grp.fromGeneratorsAndOrder(generators.map(_.toPermutation[Q]), order)
-*/
 }
 
 object Grp {
---
+++ core/src/main/scala/net/alasc/perms/PermutationActionGrpBuilder.scala
@@ -0,0 +1,5 @@
+package net.alasc.perms
+
+trait PermutationActionGrpBuilder[G] {
+
+}
